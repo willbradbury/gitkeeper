7565a3909ea3e4840d585febbabd841695cc924e
Wang Alex <Wang@rescomp-16-332627.stanford.edu>
2016-10-28 22:54:17 -0700

    downloads into train folder. will encounter 404 errors

--- repo.py
+++ repo.py
@@ -2,26 +2,35 @@
     (c) Alex Wang, Shivaal Roy, Will Bradbury
 """
 import os
+import util
 
 default_repo_root = os.environ['HOME']+"/gitkeeper/repos/"
 
 class Repo(object):
   def __init__(self, name, root=default_repo_root):
     self.name = name
-    self.repoName = self.name.split('/')[-1]
+    self.id = self.name.replace('/', '.')
     self.exists = True
-    if not os.path.exists(default_repo_root + self.repoName):
+    if not os.path.exists(default_repo_root + self.id):
         self.exists = False
 
 class RemoteRepo(object):
   def __init__(self, name):
     self.name = name
+    self.id = self.name.replace('/', '.')
 
   def download(self):
     """download from github"""
-    if not os.path.exists(default_repo_root):
-        os.makedirs(default_repo_root)
-    os.chdir(default_repo_root)
+    util.createAndCd(default_repo_root)
+    util.createAndCd(self.id)
+    util.createAndCd('train')
+    # download train data
+    util.downloadPullRequests(self.name, 40000, 40001)
+    util.go_to_parent()
+    util.createAndCd('test')
+    # download test data
+    #util.downloadPullRequests(self.name, 18381,)
+    util.go_to_parent()
     os.system("git clone git@github.com:"+ self.name + ".git")
     return self
 
--- util.py
+++ util.py
@@ -2,6 +2,9 @@
     (c) Alex Wang, Shivaal Roy, Will Bradbury
 """
 from repo import Repo, RemoteRepo
+import urllib2
+import os
+import json
 
 def download(repo_name):
   """ Try to open |repo_name| locally, but if it doesn't exist,
@@ -11,4 +14,22 @@ def download(repo_name):
       return RemoteRepo(name=repo_name).download().getRepo()
   else:
       return rp
-  print rp.name
+
+def createAndCd(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+
+def downloadPullRequests(repo_name, start_index, end_index):
+  for diff in xrange(start_index, end_index):
+    # urllib2.urlretrieve('http://api.github.com/repos/' + repo_name + '/pulls/' + str(diff), str(diff) + '.metadata')
+    # urllib2.request.retrieve()
+    open(str(diff) + '.metadata', 'wrb').write(urllib2.urlopen(
+       'http://api.github.com/repos/' + repo_name + '/pulls/' + str(diff)).read())
+    open(str(diff) + '.diff', 'wrb').write(urllib2.urlopen(
+       'http://www.github.com/' + repo_name + '/pull/' + str(diff) + '.diff').read())
+    print 'http://api.github.com/repos/' + repo_name + '/pulls/' + str(diff)
+    print 'http://www.github.com/' + repo_name + '/pull/' + str(diff) + '.diff'
+
+def go_to_parent():
+  os.chdir('../')
