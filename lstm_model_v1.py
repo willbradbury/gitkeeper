fba681b0045e158b0d87e24c1a8b14d73154b4de
Will Bradbury <willbradbury@gmail.com>
2016-11-09 22:59:19 -0800

    more framework code and new perplexity computation

--- lstm_model_v1.py
+++ lstm_model_v1.py
@@ -4,34 +4,82 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import CountVectorizer
-import json, util, random
-import model
+import json, util, random, model
+from lstm_trainer import LSTMTrainer
 
 class LSTMModel(model.Model):
   def __init__(self, repo, v=1):
     self.v = v
-    self.diff_tokenizer = DiffTokenizer(v=v)
-    self.repo_tokenizer = RepoTokenizer(v=v)
-    self.token_embedder = TokenEmbedder(1000, v=v)
+    self.file_tokenizer = FileTokenizer(v=v)
+    self.diff_tokenizer = DiffTokenizer(self.file_tokenizer, v=v)
+    self.repo_tokenizer = RepoTokenizer(self.file_tokenizer, v=v)
+    self.token_embedder = TokenEmbedder(embed_size=1000, v=v)
     self.repo = repo
 
   def train(self):
     util.log(self.v, 1, "beginning training on " + self.repo.name)
-    self.train_set = list(self.embedder.embed(self.repo_tokenizer.tokenize(self.repo)))
-    self.dev_set = list(self.embedder.embed(self.diff_tokenizer.tokenize(self.repo, 'train')))
+
+    # tokenize the repo and the training diffs
+    self.train_set = [Variable(i) \
+        for i in self.embedder.embed(self.repo_tokenizer.tokenize(self.repo))]
+    self.dev_set = [Variable(i) \
+        for i in self.embedder.embed(self.diff_tokenizer.tokenize(self.repo))]
+
+    # train the rnn on the repo, reporting dev error along the way
     self.lstm_trainer = LSTMTrainer(self.train_set, self.dev_set)
     self.lstm_trainer.get_trainer().run()
+
+    # learn svm on the perplexity feature
+    x,y = np.array([]), np.array([])
+    for i,pid in enumerate(self.repo.getExamples(inTraining=True)):
+      diff_f = self.repo.getDiffFile(pid, inTraining=True)
+      x = np.append(x, self.get_perplexity(diff_f))
+
+      meta_f = self.repo.getMetaFile(pid, inTraining=True)
+      meta_json = json.load(meta_f)
+      y = np.append(y,self.extractor.label(meta_json), axis=0)
+    X = np.transpose(x) # may not be necessary... TODO: check
+    self.clf.fit(X,y)
+    score = self.clf.score(X,y)
+      
     util.log(self.v, 1, "mean training accuracy: " + str(score))
 
   def test(self):
     util.log(self.v, 1, "beginning testing on " + self.repo.name)
-    pass
+    x,y = np.array([]), np.array([])
+    for i,pid in enumerate(self.repo.getExamples(inTraining=False)):
+      diff_f = self.repo.getDiffFile(pid, inTraining=False)
+      x = np.append(x, self.get_perplexity(diff_f))
+
+      meta_f = self.repo.getMetaFile(pid, inTraining=False)
+      meta_json = json.load(meta_f)
+      y = np.append(y,self.extractor.label(meta_json), axis=0)
+
+    X = np.transpose(x) # necessary? TODO: check
+    score = self.clf.score(X,y)
     util.log(self.v, 1, "mean testing accuracy: " + str(score))
 
-class DiffTokenizer(object):
-  """A class to tokenize diff files"""
+  def get_perplexity(self, diff):
+    """computes the loss when trying to predict the next token from each token
+    in |diff|."""
+    diff_tokens = list(self.embedder.embed(self.file_tokenizer.tokenize(diff)))
+    volatile_tokens = [Variable(tok_id, volatile='on') for tok_id in diff_tokens]
+    return self.lstm_trainer.compute_loss(volatile_tokens)
+
+class FileTokenizer(object):
+  """A class to tokenize files"""
   def __init__(self, v):
     self.v = v
+
+  def tokenize(self, f):
+    # tokenize using the yield operator
+    return
+
+class DiffTokenizer(object):
+  """A class to tokenize a collection of diffs using a file tokenizer"""
+  def __init__(self, ft, v):
+    self.v = v
+    self.ft = ft
 
   def tokenize(self, repo, subset):
     util.log(self.v, 3, "tokenizing diffs in "+repo.name+"/"+subset)
@@ -39,9 +87,10 @@ class DiffTokenizer(object):
     return
 
 class RepoTokenizer(object):
-  """A class to tokenize repo files"""
-  def __init__(self, v):
+  """A class to tokenize an entire repo using a file tokenizer"""
+  def __init__(self, ft, v):
     self.v = v
+    self.ft = ft
 
   def tokenize(self, repo):
     util.log(self.v, 3, "tokenizing repo " + repo.name)
@@ -57,5 +106,4 @@ class TokenEmbedder(object):
   def embed(self, token_stream):
     util.log(self.v, 3, "embedding stream")
     for token in token_stream:
-      yield hash(token) % self.size #TODO: CHANGE THIS!!!
-    return
+      yield np.array(hash(token) % self.size) #TODO: CHANGE THIS!!!
